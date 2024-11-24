44298079d2df0c80ecae68b98e9db54f31cdf034
piumal302 <piumal302@gmail.com>
2024-11-24 14:42:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "../assets/css/styles.css";
 import Animal from "./components/Animal";
 import AnimalsGrid from "./components/AnimalsGrid";
+import Result from "./components/Result";
 
 export default App;
